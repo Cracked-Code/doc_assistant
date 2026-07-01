d8b7b703c18410b256c4946f8168c96079e013b8
Nick <nickalot03@gmail.com>
2026-07-01 02:36:49 -0400

    Updated /health for mointoring 2

--- backend/Routes/health.py
+++ backend/Routes/health.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter, Depends, Request
 
 router = APIRouter()
 
-@router.get("/health", methods=["GET", "HEAD"])
+@router.api_route("/health", methods=["GET", "HEAD"])
 def health():
-    return {"status":"ok"}
+    return {"status": "ok"}
