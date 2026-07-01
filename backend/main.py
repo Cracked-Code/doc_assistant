8aabbe703914ef3540e31212b4ed3487985c42e2
Nick <nickalot03@gmail.com>
2026-06-30 22:34:25 -0400

    update main to allow production

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,15 @@ from Routes.query import router2
 from Routes.ingest import router1
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(","),
     allow_methods=["*"],
     allow_headers=["*"],
 )
